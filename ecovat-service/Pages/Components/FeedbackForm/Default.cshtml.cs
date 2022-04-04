d6efe85918f4b3fab0ea584c6dffeb53bbace495
Anton.Drozdov <anton.drozdov@arcadia.spb.ru>
2022-04-04 11:38:41 +0300

    Spring 157: [Vega] US47109: [SPIKE][Vega] HTML Animations as Source Material in HTML Web delivery

---
+++ ecovat-service/Pages/Components/FeedbackForm/Default.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ecovat_service.Pages.Components.FeedbackForm
+{
+    public class DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
